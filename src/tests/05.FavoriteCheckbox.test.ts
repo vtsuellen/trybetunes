eea59b13da152d40e43878ef22011762dcd5fd55
Arthur Procópio <avprocopio@gmail.com>
2023-06-15 11:09:33 -0300

    Update 05.FavoriteCheckbox.test.ts

--- src/tests/05.FavoriteCheckbox.test.ts
+++ src/tests/05.FavoriteCheckbox.test.ts
@@ -49,7 +49,7 @@ describe('5 - Crie o mecanismo para adicionar músicas na lista de músicas favo
 
       const checkbox = screen.getByTestId('checkbox-music-31');
 
-      const img = within(checkbox).getByRole('img');
+      const img = within(checkbox).getByRole('presentation');
       expect(img).toHaveAttribute('src', '/src/images/empty_heart.png');
 
       userEvent.click(checkbox);
